685f1f6933b0c4bae056a7aeb9e3843fc2becd19
TimLuq <timluq+gitcommit@gmail.com>
2023-04-03 01:08:19 +0200

    remove built files from git

--- umd/emmet-slot.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A slot in the generated emmet structure.
- * Although this is an element, use only the methods and properties that manipulate its children.
- */
-export declare class EmmetSlot extends HTMLElement {
-    connectedCallback(): void;
-}
--- umd/emmet-slot.js
+++
@@ -1,2 +0,0 @@
-var e,t;e=this,t=function(e){class t extends HTMLElement{connectedCallback(){const e=this.style;e.display||(e.display="contents")}}"object"==typeof customElements&&customElements.define("emmet-slot",t),e.EmmetSlot=t},"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e="undefined"!=typeof globalThis?globalThis:e||self).emmetSlot={});
-//# sourceMappingURL=emmet-slot.js.map
